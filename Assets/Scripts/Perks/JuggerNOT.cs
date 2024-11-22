4540de2f8d06f783a0bd1833ed0eabd471d3538c
NekoZecho <zacharymunn07@gmail.com>
2024-11-21 20:56:31 -0800

    Updated

--- Assets/Scripts/Perks/JuggerNOT.cs
+++ Assets/Scripts/Perks/JuggerNOT.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class JuggerNOT : MonoBehaviour
@@ -12,6 +13,9 @@ public class JuggerNOT : MonoBehaviour
     bool hasPerk = false;
     GameObject player;
     GameObject pointHandler;
+    bool inRange = false;
+    [SerializeField]
+    TMP_Text pointcost;
 
     private DavidHealth health;
     private Points points;
@@ -23,12 +27,42 @@ public class JuggerNOT : MonoBehaviour
         health = player.GetComponent<DavidHealth>();
         pointHandler = GameObject.FindGameObjectWithTag("PointHandler");
         points = pointHandler.GetComponent<Points>();
+        bool inRange = false;
+        pointcost.enabled = false;
 
     }
 
+    private void Update()
+    {
+        if (inRange == true)
+        {
+            JuggerNogBuy();
+        }
+    }
+
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == ("Player") && health.interact == true && points.currentPoints >= perkCost && hasPerk == false)
+        if (collision.gameObject.tag == ("Player"))
+        {
+            pointcost.text = "Perk Cost: " + perkCost.ToString();
+            pointcost.enabled = true;
+            bool inRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == ("Player"))
+        {
+            pointcost.enabled = false;
+            bool inRange = false;
+        }
+    }
+
+    public void JuggerNogBuy()
+    {
+        if (inRange == true && health.interact == true && points.currentPoints >= perkCost && hasPerk == false)
         {
             points.currentPoints -= perkCost;
             health.maxHealth += healthIncrease;
@@ -36,5 +70,4 @@ public class JuggerNOT : MonoBehaviour
             hasPerk = true;
         }
     }
-
 }
